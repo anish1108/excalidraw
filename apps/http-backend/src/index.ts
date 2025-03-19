db3a1dbf1a1ea1b0207587d351fae95c89924af3
anish11 <anishprasad2512@gmail.com>
2025-03-19 21:40:26 +0530

    added common package with zod schema

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express"
 import jwt from "jsonwebtoken"
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backendcommon/config";
+import { UserSchema, SigninSchema, RoomSchema } from "@repo/common/types"
+
 
 
 const app = express();
@@ -14,6 +16,14 @@ app.post("/signup",(req, res)=>{
     const email = req.body.email;
     const password = req.body.password;
 
+    const data = UserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect data"
+        })
+        return;
+    }
+
     // zod validation
     res.send("signup")
     
@@ -22,6 +32,14 @@ app.post("/signup",(req, res)=>{
 app.post("/signin", (req, res)=>{
     const username = req.body.username;
     const password = req.body.password;
+
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "Incorrect data"
+        })
+        return;
+    }
 
     // db call
 
@@ -36,6 +54,14 @@ app.post("/signin", (req, res)=>{
 })
 
 app.post("/room", middleware, (req, res)=>{
+
+    const data = RoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message: "Incorrect data"
+        })
+        return;
+    }
     res.send("create room")
 })
 
