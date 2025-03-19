b72c590a1ce5a2210488f7bfa456c94321308ea4
anish11 <anishprasad2512@gmail.com>
2025-03-19 15:40:12 +0530

    added http server and ws server

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,44 @@
 import express from "express"
+import jwt from "jsonwebtoken"
+import { middleware } from "./middleware";
+import { JWT_SECRET } from "./config";
+
 
 const app = express();
 
-app.listen(3001);
+app.use(express.json());
+
+app.post("/signup",(req, res)=>{
+    res.send("signup")
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // zod validation
+    res.send("signup")
+    
+})
+
+app.post("/signin", (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // db call
+
+    
+    const token = jwt.sign({
+        username
+    },JWT_SECRET)
+    
+
+    res.send(token)
+
+})
+
+app.post("/room", middleware, (req, res)=>{
+    res.send("create room")
+})
+
+app.listen(3001,()=>{
+    console.log("Port is started on 3000")
+});
